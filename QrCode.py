5f610b58e0fe83d11f2b3dcb0d95abfd36434b74
VVroli <gaisinagaika@gmail.com>
2023-06-04 20:24:37 +0300

    типо на 4

--- QrCode.py
+++ QrCode.py
@@ -1,3 +1,26 @@
-if __name__ == "__main__":
-    pass
-#qswqs
+import numpy as np
+
+
+def qr_eigenvalues(matrix, iterations=100):
+    n = matrix.shape[0]
+    eigenvalues = np.zeros(n)
+    for _ in range(iterations):
+        Q, R = np.linalg.qr(matrix)
+        matrix = np.dot(R, Q)
+
+    for i in range(n):
+        eigenvalues[i] = matrix[i, i]
+
+    return eigenvalues
+
+
+# Пример использования
+# Создаем квадратную матрицу
+A = np.array([[3, -1, 0], [-1, 2, -1], [0, -1, 1]])
+
+# Находим собственные числа
+eigenvalues = qr_eigenvalues(A)
+
+# Выводим результаты
+print("Собственные числа матрицы A:")
+print(eigenvalues)
