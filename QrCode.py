00034850baa0059122d09179c7e43776412b69cc
Андрей <elkabor@mail.ru>
2023-06-05 18:35:11 +0300

    на 6-7

--- QrCode.py
+++ QrCode.py
@@ -1,2 +1,19 @@
-if __name__ == "__main__":
-    print('fgu sj gi')
+import numpy as np
+
+def eigenvalues_qr(matrix, iterations=100):
+    m, n = matrix.shape
+    eigenvalues = []
+
+    for _ in range(iterations):
+        Q, R = np.linalg.qr(matrix)
+        matrix = np.dot(R, Q)
+
+    for i in range(n):
+        eigenvalues.append(matrix[i, i])
+
+    return eigenvalues
+
+# Пример использования
+matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+eigenvalues = eigenvalues_qr(matrix)
+print("Собственные числа:", eigenvalues)
