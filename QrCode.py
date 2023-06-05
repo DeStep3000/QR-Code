700fa5e46b3cb53496253488883710d9095feecb
Андрей <elkabor@mail.ru>
2023-06-05 19:02:06 +0300

    на 8 спиздил у алины

--- QrCode.py
+++ QrCode.py
@@ -1,26 +1,39 @@
 import numpy as np
 
-def qr_decomposition(matrix):
-    m, n = matrix.shape
-    Q = np.eye(m)
-    R = np.copy(matrix)
+def qr_decomposition(A):
+    m, n = A.shape
+    Q = np.eye(m)  # Инициализация матрицы Q как единичной матрицы размерности m x m
+    R = A.copy()  # Копирование исходной матрицы A в матрицу R
 
     for j in range(n):
-        column = R[j:, j]
-        norm = np.linalg.norm(column)
-        sign = np.sign(column[0])
-        v = column + sign * norm * np.eye(len(column))[:, 0]
-        v = v / np.linalg.norm(v)
+        # Построение матрицы отражения H_j
+        x = R[j:, j]  # Выбираем столбец матрицы R, начиная с позиции j
+        e = np.zeros_like(x)
+        e[0] = 1  # Вектор e = (1, 0, ..., 0)
 
-        R[j:, :] -= 2.0 * np.outer(v, np.dot(v.T, R[j:, :]))
-        Q[:, j:] -= 2.0 * np.outer(Q[:, j:], np.dot(Q[:, j:].T, v))
+        u = np.sign(x[0]) * np.linalg.norm(x) * e + x  # Вектор u = sign(x[0]) * ||x|| * e + x
+        u = u / np.linalg.norm(u)  # Нормализация вектора u
+
+        # Построение матрицы отражения H_j = I - 2uu^T
+        H = np.eye(m)
+        H[j:, j:] -= 2.0 * np.outer(u, u)
+
+        # Применение матрицы отражения H_j к матрицам Q и R
+        Q = np.dot(Q, H.T)
+        R = np.dot(H, R)
 
     return Q, R
+A = np.array([[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]])
 
-# Пример использования
-matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-Q, R = qr_decomposition(matrix)
+Q, R = qr_decomposition(A)
+
 print("Матрица Q:")
 print(Q)
 print("Матрица R:")
 print(R)
+res = np.dot(Q, R)
+print("Матрица A:")
+print(res)
+
