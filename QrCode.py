6d24a06bd61251888e8fb6dc995db8ed7b4a4cb4
Андрей <elkabor@mail.ru>
2023-06-13 20:43:40 +0300

    На 8 почти сделал(не работает пока+ добавить комплексные числа) Ну и сделать все красиво надо

--- QrCode.py
+++ QrCode.py
@@ -1,53 +1,95 @@
 import numpy as np
 
-def read_matrix():
-    matrix = []
-    size = int(input("Введите размер квадратной матрицы: "))
+epsilon=1e-10
+def qr_decomposition(A):
+    m, n = A.shape
+    Q = np.eye(m)  # Инициализация матрицы Q как единичной матрицы размером m x m
+    R = np.copy(A)  # Создание копии исходной матрицы A
 
-    print("Введите элементы матрицы:")
+    for j in range(n):
+        # Вычисление вектора отражения
+        x = R[j:, j]
+        norm_x = np.linalg.norm(x)
+        v = np.zeros_like(x)
+        v[0] = x[0] + np.copysign(norm_x, x[0])
+        v[1:] = x[1:]
 
-    for i in range(size):
-        row = []
-        for j in range(size):
-            element = int(input(f"Введите элемент [{i+1}, {j+1}]: "))
-            row.append(element)
-        matrix.append(row)
+        # Применение преобразования Хаусхолдера к матрицам Q и R
+        H = np.eye(m)
+        H[j:, j:] -= 2.0 * np.outer(v, v) / np.dot(v, v)
+        Q = np.dot(Q, H)
+        R = np.dot(H, R)
 
-    alpha= int(input('Введите 1, если собственные значения вещественные, 2-комлексно-сопряженные, 3-кратные, 4-комплексно-сопряженнные кратные: '))
-    p=0
-    if (alpha==3 or alpha==4):
-        p=int(input('Введите число, которому собственные значения кратны: '))
-
-    return matrix, alpha,p
-
-def eigenvalues_qr(matrix, alpha,p, iterations=100):
-    m, n = matrix.shape
-    eigenvalues = []
-
-    for _ in range(iterations):
-        Q, R = np.linalg.qr(matrix)
-        matrix = np.dot(R, Q)
-
-    if alpha==1:
-        for i in range(n):
-            eigenvalues.append(matrix[i, i])
-
-    if alpha == 2:
-        i=n-2
-        eigenvalues.append(matrix[i:,i:])
-
-    if alpha == 3:
-        i=n-p
-        eigenvalues.append(matrix[i:, i:])
-
-    if alpha == 4:
-        i = n - 2*p
-        eigenvalues.append(matrix[i:, i:])
-    return eigenvalues
+    return Q, R
 
 # Пример использования
-#matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-matrix,alpha,p = read_matrix()
-print(matrix)
-eigenvalues = eigenvalues_qr(matrix,alpha,p)
-print("Собственные числа:", eigenvalues)
+A = np.array([[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]])
+
+Q, R = qr_decomposition(A)
+A_res= np.dot(Q, R)
+
+print("Матрица Q:")
+print(Q)
+print("Матрица R:")
+print(R)
+A_res= np.dot(Q, R)
+print("Матрица A_res:")
+print(A_res)
+
+def qr_algorithm(A, max_iterations=1000):
+    n = A.shape[0]
+
+    for i in range(max_iterations):
+        Q, R = qr_decomposition(A)
+        A = np.dot(R, Q)
+
+        # Проверяем условие сходимости
+        off_diag_sum = np.sum(np.abs(A - np.diag(np.diagonal(A))))
+        if off_diag_sum < epsilon:
+            break
+
+    return A
+
+A_k=qr_algorithm(A)
+
+def find_eigenvalues(A):
+    eigenvalues = []
+    n = A.shape[0]
+    flag=0
+
+    for i in range(n-1):
+        if flag==1:
+            flag=0
+            continue
+        if abs(A[i+1][i])<epsilon:
+            flag = 0
+            eigenvalues.append(A[i][i])
+            continue
+
+        a1=A[i][i]
+        b1=A[i][i+1]
+        c1=A[i+1][i]
+        d1=A[i+1][i+1]
+
+        a2=1
+        b2=-a1-d1
+        c2=a1*d1-b1*c1
+        d=b2*b2-4*a2*c2
+
+        if d==0:
+            x1=-b2/2
+            eigenvalues.append(x1)
+
+        if d<0:
+            x1=-b2/2+sqrt(-d)*IMAG /2#как прописать в питоне эти мнимые числа как числа хз
+            x1 = -b2 / 2 - sqrt(-d)*IMAG / 2
+
+    return eigenvalues
+
+eigenvalues_res = find_eigenvalues(A)
+
+print('eigenvalues:')
+for eigenvalue in eigenvalues_res:
+    print(eigenvalue)
