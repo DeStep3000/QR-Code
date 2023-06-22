91bb3554883c81ea94a3d8cb70370322e52ddf00
DeStep <tankt34.stepanoff@yandex.ru>
2023-06-22 12:16:57 +0300

    TO BE CONTINUED(6 - 7)

--- QrCode.py
+++ QrCode.py
@@ -3,46 +3,25 @@ from math import sqrt
 
 epsilon = 1e-30
 
+# Функция для чтения квадратной матрицы из пользовательского ввода
 def read_square_matrix():
-    size = int(input("Введите размерность квадратной матрицы: "))
+    size = int(input("Введите размерность квадратной матрицы: "))  # Запрашиваем размерность квадратной матрицы у пользователя
     matrix = []
-    print("Введите элементы матрицы построчно:")
+    print("Введите элементы матрицы:")
     for _ in range(size):
         row = []
         for _ in range(size):
-            element = int(input())
+            element = int(input())  # Запрашиваем элементы матрицы от пользователя
             row.append(element)
         matrix.append(row)
     return matrix, size
 
-def qr_decomposition(A,size):
-    m = size
-    n = size
-    Q = np.eye(m)  # Инициализация матрицы Q как единичной матрицы размером m x m
-    R = np.copy(A)  # Создание копии исходной матрицы A
-
-    for j in range(n):
-        # Вычисление вектора отражения
-        x = R[j:, j]
-        norm_x = np.linalg.norm(x)
-        v = np.zeros_like(x)
-        v[0] = x[0] + np.copysign(norm_x, x[0])
-        v[1:] = x[1:]
-
-        # Применение преобразования Хаусхолдера к матрицам Q и R
-        H = np.eye(m)
-        H[j:, j:] -= 2.0 * np.outer(v, v) / np.dot(v, v)
-        Q = np.dot(Q, H)
-        R = np.dot(H, R)
-
-    return Q, R
-
-
+# Функция для выполнения QR-алгоритма над матрицей A
 def qr_algorithm(A, size, max_iterations=100000):
     n = size
 
     for i in range(max_iterations):
-        Q, R = qr_decomposition(A,size)
+        Q, R = np.linalg.qr(A)
         A = np.dot(R, Q)
 
         # Проверяем условие сходимости
@@ -51,6 +30,7 @@ def qr_algorithm(A, size, max_iterations=100000):
             break
     return A
 
+# Функция для нахождения собственных значений матрицы A
 def find_eigenvalues(A):
     eigenvalues = []
     n = A.shape[0]
@@ -75,30 +55,28 @@ def find_eigenvalues(A):
         c2 = a1 * d1 - b1 * c1
         d = b2 ** 2 - 4 * a2 * c2
 
-        if np.abs(d)<epsilon:
+        if np.abs(d) < epsilon:
             x1 = -b2 / 2
             eigenvalues.append(x1)
 
         else:
-            x1 = complex(-b2 / 2, sqrt(np.abs(d)) / 2)  # как прописать в питоне эти мнимые числа как числа хз
+            x1 = complex(-b2 / 2, sqrt(np.abs(d)) / 2)
             x2 = complex(-b2 / 2, -sqrt(np.abs(d)) / 2)
             eigenvalues.extend([x1, x2])
-
 
     if flag != 1:
         eigenvalues.append(A[-1][-1])
 
     return eigenvalues
 
-
 if __name__ == "__main__":
-    #A = np.array([[0, 0, 2],
+    # A = np.array([[0, 0, 2],
     #              [1, 0, -5],
     #              [0, 1, 4]])
 
-    A,size=read_square_matrix()
+    A, size = read_square_matrix()
 
-    Q, R = qr_decomposition(A,size)
+    Q, R = np.linalg.qr(A)
 
     print("Матрица Q:")
     print(Q)
@@ -108,13 +86,10 @@ if __name__ == "__main__":
     print("Матрица A_res:")
     print(np.dot(Q, R))
 
-    A_k = qr_algorithm(A,size)
+    A_k = qr_algorithm(A, size)
     print("Матрица A_k:")
     print(A_k)
 
-    print('eigenvalues:')
+    print('Собственные значения:')
     for eigenvalue in find_eigenvalues(A_k):
         print(eigenvalue)
-
-
-
